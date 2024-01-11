e2a5fce07a4814eb7c4dcfa687556000ab78462c
Sreegith Aravind <sreegith.aravind@emvigotech.com>
2024-01-11 20:36:53 +0530

    sai website done

---
+++ assets/js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".bannerSwiper", {
+        direction: "vertical",
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        mousewheel: {
+          releaseOnEdges: true
+        }
+      });
